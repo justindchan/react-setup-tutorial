e3c094a83cc5322ece7086da8a14d0dd619f06d8
Justin <justindchan@gmail.com>
2020-05-10 23:32:19 -0700

    installed and configured babel to run with webpack. app is loading in browser.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: '.src//app.js',
+  entry: './src/app.js',
   output: {
-    path: path.join(_dirname, 'public'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
   }
 };
